08cee9062595315f594284aea0bbf0596ba0fcae
Silviu-Leonard Vatamanelu <silviu48vatamanelu@gmail.com>
2023-06-10 15:26:44 +0200

    How naive

--- apollo_tms/start.sh
+++ apollo_tms/start.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations
 python manage.py migrate
 
 python manage.py collectstatic --noinput
-/root/.local/bin/gunicorn apollo_tms.wsgi:application --bind=0.0.0.0:8000 --workers=4
+/root/.local/bin/gunicorn apollo_tms.wsgi:application --bind=0.0.0.0:$PORT --workers=4
