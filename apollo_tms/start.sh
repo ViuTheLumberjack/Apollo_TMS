efa96119f5d5c2f59a216e41019015a259254188
Silviu-Leonard Vatamanelu <silviu48vatamanelu@gmail.com>
2023-06-10 15:11:57 +0200

    God what have i done wrong?

--- apollo_tms/start.sh
+++ apollo_tms/start.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations
 python manage.py migrate
 
 python manage.py collectstatic --noinput
-/root/.local/bin/gunicorn apollo_tms.wsgi:application --bind=0.0.0.0:8000 --workers=4 --log-file 
+/root/.local/bin/gunicorn apollo_tms.wsgi:application --bind=0.0.0.0:8000 --workers=4
